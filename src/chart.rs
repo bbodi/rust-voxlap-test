dc7ace9214f117251265c7dcff751f5da5adb309
bbodi <bodi.balazs@multi-pass.hu>
2014-10-25 17:18:07 +0200

    fancier chart

--- src/chart.rs
+++ src/chart.rs
@@ -62,16 +62,37 @@ impl Chart {
     }
 
     fn draw_bars(&self, dst: &RenderContext) {
+        let mut last_value = 0f32;
         for iter in self.data.iter().enumerate() {
             let (index, value) = iter;
+            let cur_value = min(*value, self.max_height);
             for i in range(0, self.column_width) {
                 let x1 = max(0, self.right_x as i32 - (index as i32*self.column_width as i32 + i as i32));
-                let y1 = self.bottom_y - min(*value, self.max_height);
+                //let y1 = self.bottom_y - min(*value, self.max_height);
+                let p = i as f32 / self.column_width as f32;
+                let value = (last_value * (1f32-p) + cur_value as f32 * p) as u32;
                 let x2 = x1;
                 let y2 = self.bottom_y;
-                dst.draw_line_2d(x1 as u32, y1, x2 as u32, y2, voxlap::Color::rgb(0, 0, 255));
+                //dst.draw_line_2d(x1 as u32, y1, x2 as u32, y2, voxlap::Color::rgb(0, 0, 255));
+                self.draw_gradient_bar(dst, x1 as u32, value, voxlap::Color::rgb(42, 42, 42), voxlap::Color::rgb(200, 200, 200))
             }
+            last_value = cur_value as f32;
         }
+    }
+
+    fn draw_gradient_bar(&self, dst: &RenderContext, x:u32, value: u32, start_color: voxlap::Color, end_color: voxlap::Color) {
+        for i in range(0, value) {
+            //let p = min(self.max_height, (y2-y)) as f32 / self.max_height as f32;
+            let p = i as f32 / self.max_height as f32;
+            //println!("cur_point_value = {}, sp = {}", cur_point_value, sp);
+            let sp = 1f32 - p;
+            let r = start_color.r as f32 * sp + end_color.r as f32 * p;
+            let g = start_color.g as f32 * sp + end_color.g as f32 * p;
+            let b = start_color.b as f32 * sp + end_color.b as f32 * p;
+            dst.draw_point_2d(x, self.bottom_y - i, voxlap::Color::rgb(r as u8, g as u8, b as u8));
+        }
+        dst.draw_point_2d(x, self.bottom_y - value, voxlap::Color::rgb(217, 137, 50));
+        dst.draw_point_2d(x, self.bottom_y - value-1, voxlap::Color::rgb(217, 137, 50));
     }
 
     fn draw_center_line(&self, dst: &RenderContext) {
